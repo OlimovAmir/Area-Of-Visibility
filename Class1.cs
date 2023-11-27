d4fab06e9bace89923e0234c70e108f0ef25a2ae
olimov Amir <olimov.amir@mail.ru>
2023-11-27 09:12:39 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Of_Visibility
+{
+     class Class1
+    {
+        static int b = 11;
+
+        static void Foo()
+        {
+            //int b = 25;
+            Console.WriteLine(b);
+        }
+        static void Main(string[] args)
+        {
+            
+             
+            for (int i = 0; i < 5; i++)
+            {
+                i = i + 1;
+                Console.WriteLine(i);   
+            }
+            Foo();
+        }
+    }
+}
